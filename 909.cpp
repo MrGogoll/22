6b94d93696f3abd307fe16cd99f7ae9ee7a79b1a
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-19 15:16:38 +0300

    Add files via upload

---
+++ 909.cpp
@@ -0,0 +1,15 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s << endl;
+	int n = s.length();
+	if (n % 2 == 0) {
+		cout << s;
+	}
+}
+
